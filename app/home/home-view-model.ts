b8e79c4ee9f045a41543ac8a7d0a775230f8828a
Shirline Serrao <shirlineserrao@shirlines-air.home>
2019-01-17 14:41:12 +0000

    folder structure organised

---
+++ app/home/home-view-model.ts
@@ -0,0 +1,23 @@
+import { Observable } from "tns-core-modules/data/observable";
+import { Item } from "./shared/item";
+
+export class HomeViewModel extends Observable {
+    // items: Array<Item>;
+
+    constructor() {
+        super();
+
+
+
+        /* this.items = new Array<Item>(
+            {
+                name: "Item 1",
+                description: "Description for Item 1"
+            },
+            {
+                name: "Item 2",
+                description: "Description for Item 2"
+            }
+        ); */
+    }
+}
